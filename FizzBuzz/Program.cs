55f193f28fa6295f91b82d4129ae20c01e7f871c
Mathew Langford <Mathew.Langford@softwire.com>
2017-07-04 16:40:55 +0100

    Implemented basic FizzBuzz program

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -10,6 +10,82 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
+            Program fizzBuzzProgram = new FizzBuzz.Program();
+            fizzBuzzProgram.PrintFizzBuzzToNInclusive(100);
+        }
+
+        private void PrintFizzBuzzToNInclusive(int n)
+        {
+            int current = 0;
+            while(current <= n)
+            {
+                PrintFizzBuzzTest(current);
+                current++;
+            }
+        }
+
+        private void PrintFizzBuzzTest(int n)
+        {
+            bool fizzResult = PrintFizzTest(n);
+            bool buzzResult = PrintBuzzTest(n);
+
+            if(NeitherFizzOrBuzz(fizzResult, buzzResult))
+            {
+                PrintInteger(n);
+            }
+
+            PrintNewline();
+        }
+
+        private bool PrintFizzTest(int n)
+        {
+            if(IsFizz(n))
+            {
+                Console.Write("Fizz");
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool PrintBuzzTest(int n)
+        {
+            if(IsBuzz(n))
+            {
+                Console.Write("Buzz");
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private bool NeitherFizzOrBuzz(bool fizz, bool buzz)
+        {
+            return !(fizz || buzz);
+        }
+
+        private void PrintInteger(int n)
+        {
+            Console.Write(n);
+        }
+
+        private void PrintNewline()
+        {
+            Console.WriteLine();
+        }
+
+        private bool IsFizz(int n)
+        {
+            return (n % 3 == 0);
+        }
+
+        private bool IsBuzz(int n)
+        {
+            return (n % 5 == 0);
         }
     }
 }
